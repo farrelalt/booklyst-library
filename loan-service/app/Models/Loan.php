b0ae4fce00238e51104f8b1b563e8949c7cdba6c
ivena <waromiivena@gmail.com>
2026-04-29 07:49:13 +0700

    add laravel loan microservice

---
+++ loan-service/app/Models/Loan.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Loan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'member_id',
+        'book_id',
+        'loan_date',
+        'return_date',
+        'status'
+    ];
+}
